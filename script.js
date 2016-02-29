4fcfc3507d9ee41412ddc8a259fbc7f665f6e40a
George sedky <georgesedky@Georges-MBP.fritz.box>
2016-02-29 22:36:57 +0100

    Implemented functionality with ntlm authentication

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var fs = require('fs');
-var request = require('request');
+var httpntlm = require('httpntlm');
 var cheerio = require('cheerio');
+var address = 'http://student.guc.edu.eg';
+var usname;
+var passw;
 
 getCredentials();
 
@@ -10,49 +13,86 @@ function getCredentials(uname,pass){
 
   if(!uname){
     stdout.write("Enter GUC username: ");
-    stdin.once('data', function(data) {
-      getCredentials(data.toString().trim(),pass);
+    stdin.once('data', function(data){
+      getCredentials(data.toString().trim(), pass);
     });
   }else if(!pass){
     stdout.write("Enter GUC password: ");
-    stdin.once('data', function(data) {
-      getCredentials(uname,data.toString().trim());
+    stdin.once('data', function(data){
+      getCredentials(uname, data.toString().trim());
     });
   }else{
-    scrap(uname, pass);
-    process.exit();
+    scrap(uname, pass, function(){process.exit();});
   }
 }
 
 function scrap(username, password){
   var auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
-  var url1 = 'http://student.guc.edu.eg/intranet/Faculties/Media%20Engineering%20Technology/GUC%20Berlin%20SS16%20CSEN%206th/';
-  var urlmgmnt = 'http://student.guc.edu.eg/intranet/Faculties/Engineering%20&%20Material%20Sciences/Mechatronics%20Engineering/GUC%20Berlin%20SS16%20MCTR%206th/Introduction%20to%20Management/';
-  console.log(username + " " + password);
-  console.log(auth);
-  getMaterials(auth, url1, __dirname);
-  getMaterials(auth, urlmgmnt, __dirname + decodeURI('/GUC%20Berlin%20SS16%20CSEN%206th'));
+  var url1 = address + '/intranet/Faculties/Media%20Engineering%20Technology/GUC%20Berlin%20SS16%20CSEN%206th/';
+  var urlmgmnt = address + '/intranet/Faculties/Engineering%20&%20Material%20Sciences/Mechatronics%20Engineering/GUC%20Berlin%20SS16%20MCTR%206th/Introduction%20to%20Management/';
+
+  usname = username;
+  passw = password;
+  getMaterials(url1, __dirname, function(){
+    getMaterials(urlmgmnt, __dirname + decodeURI('/GUC%20Berlin%20SS16%20CSEN%206th'));
+  });
+
 }
 
-function getMaterials(auth, myUrl, rootDir){
+function getMaterials(myUrl, parentDir){
 
   var temp = myUrl.split('/');
-  a.pop();
-  var newDir = '/' + decodeURI(a.pop());
 
-  request(
+  httpntlm.get({
+      url: myUrl,
+      username: usname,
+      password: passw,
+  }, function (err, res){
+    if(!err){
+
+      var isHtmlDoc = /text\/html/.test(res.headers['content-type']);
+
+      if(isHtmlDoc){
+        var $ = cheerio.load(res.body);
+        var name = decodeURI( temp[temp.length-2]);
+        var newPath = parentDir + '/' + name;
+
+        $('a').each(function(i, elem){
+          if( !(/To Parent Directory/.test(elem.children[0].data)) )
           {
-            url : myUrl,
-            headers : {
-                "Authorization" : auth
+            if (!fs.existsSync(newPath)){
+                fs.mkdirSync(newPath);
             }
-          },
-           function(error, response, html){
-              if(!error){
-                  var $ = cheerio.load(html);
+            getMaterials(address + elem.attribs.href, newPath);
+          }
+        });
+      }else{
+        if (!fs.existsSync(newPath)){
+          var name = decodeURI( temp[temp.length-1]);
+          var newPath = parentDir + '/' + name;
 
-                  $('a')
-                }
+          // var resumer = require('resumer');
+          // var buffer = new Buffer(res.body);
+          // var stream = resumer().queue(buffer).end();
+          // var file = fs.createWriteStream(newPath);
+          // stream.pipe(file);
+          // stream.on('error', function(err) { console.log(err); });
+          // stream.on('finish', function() { file.close(cb) });
+
+          fs.writeFile(newPath, res.body, function(err1){
+            if(err1)
+            {
+              console.log(err1);
+            }else {
+              console.log("downloaded " + name);
+              console.log(res);
             }
-  );
+          });
+        }
+      }
+    }else{
+      console.log(err);
+    }
+  });
+
 }
